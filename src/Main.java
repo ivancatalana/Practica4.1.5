e107c46df6ea061cd51950d0c5f1ada4073c0139
ivancatalana <ivancatalana@gmail.com>
2022-10-10 21:19:57 +0200

    First implementation Menu & Main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-
-
+        Menu menu =new Menu();
+       boolean menuOpt=false;
+       while (!menuOpt){
+           if (menu.principal()==1){
+               if (menu.producte()==1)menu.alimentacio();
+           }
+           else if (menu.principal()==2) ;
+           else if (menu.principal()==3)menuOpt=true;
+           else if (menu.principal()==4)menuOpt=true;
+       }
     }
-
-
-
-
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,42 @@
-public class Menu {
+import java.util.Scanner;
 
+public class Menu {
+        Scanner scanner=new Scanner(System.in);
+       public int principal(){
+           System.out.printf("1. Introduir producte\n" +
+                   "2. Passar per caixa\n"+
+                   "3. Mostrar carro de la compra\n"+
+                   "4. Sortir\n"
+           );
+           int option= scanner.nextInt();
+           scanner.nextLine();
+           return option;
+       }
+       public int producte(){
+           System.out.printf("1. Alimentació\n" +
+                   "2. Tèxtil\n"+
+                   "3. Electrònica\n"+
+                   "4. Tornar\n"
+           );
+           int option = scanner.nextInt();
+           scanner.nextLine();
+           return option;
+       }
+       public Alimentacio alimentacio (){
+           System.out.printf("Preu?");
+           int preu=scanner.nextInt();
+           scanner.nextLine();
+           System.out.printf("Nom?");
+           String nom=scanner.nextLine();
+           System.out.printf("Codi de barres?");
+           int codiDeBarres=scanner.nextInt();
+           scanner.nextLine();
+           System.out.printf("Data de caducitat?");
+           int diesDeCaducitat=scanner.nextInt();
+           Alimentacio aliment = new Alimentacio(preu,nom,codiDeBarres,diesDeCaducitat);
+           return aliment;
+
+
+
+       }
 }
